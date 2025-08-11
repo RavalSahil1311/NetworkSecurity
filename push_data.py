9d6b0e96b60152641e5e99c37b1235146437d479
Raval Sahil <ravalsahil1311@gmail.com>
2025-08-11 09:41:03 +0530

    started working on data ingestion script

--- push_data.py
+++ push_data.py
@@ -28,5 +28,35 @@ class NetworkDataExtract:
         try:
             data = pd.read_csv(file_path)
             data.reset_index(drop=True, inplace=True)
+            records = list(json.loads(data.T.to_json()).values())
+            return records
         except Exception as e:
             raise NetworkSecurityException(e, sys)
+
+    def insert_data_to_mongo(self, records, database, collection):
+        try:
+            self.database = database
+            self.collection = collection
+            self.records = records
+
+            self.pymongo_client = pymongo.MongoClient(MONGO_DB_URL)
+            self.database = self.pymongo_client[self.database]
+            self.collection = self.database[self.collection]
+            self.collection.insert_many(self.records)
+            return len(self.records)
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+
+
+if __name__ == "__main__":
+    FILE_PATH = "Network_Data\phisingData.csv"
+    DATABASE = "NetworkSecurity"
+    COLLECTION = "NetworkData"
+    network_obj = NetworkDataExtract()
+    records = network_obj.csv_to_json_converter(file_path=FILE_PATH)
+    no_records = network_obj.insert_data_to_mongo(
+        records=records,
+        database=DATABASE,
+        collection=COLLECTION,
+    )
+    print(f"Total records inserted: {no_records}")
